2391a50265d8ac3e27defa593f70c9d3fcae5a35
student12 <student12@itpark.ru>
2017-09-25 21:00:59 +0300

    add classworks

---
+++ Tasks/Task3/src/ru/itpark/Main.java
@@ -0,0 +1,19 @@
+package ru.itpark;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arraySize = scanner.nextInt();
+        int array []=new int[arraySize];
+        int min = array[0];
+        for (int = 1; i < array.length; i++){
+            if array [i] < min {
+                min = array [i]
+            }
+        }
+System.out.println(min);
+    }
+}
